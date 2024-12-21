2eb5a281f80bb774229be8a27100e9a8fe0c0c17
Mujaddid Fathi Athoillah <mujaddid201@gmail.com>
2024-12-21 08:07:15 +0700

    Fix bugs

--- app/src/main/java/com/mujaddidfa/stegonest/MainActivity.kt
+++ app/src/main/java/com/mujaddidfa/stegonest/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.mujaddidfa.stegonest
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +16,7 @@ import kotlin.system.exitProcess
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("ObsoleteSdkInt")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,24 +26,24 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_PERMISSION)
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                 REQUEST_PERMISSION)
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                 REQUEST_PERMISSION)
         }
 
         binding.cardEncrypt.setOnClickListener {
-            intent= Intent(this, EncryptActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, EncryptActivity::class.java))
         }
 
         binding.cardDecrypt.setOnClickListener {
-            intent= Intent(this, DecryptActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, DecryptActivity::class.java))
         }
     }
 
